c6d7f001aa70510452635d93a82de754b832fdbc
abinjospj <abinjospj0722@gmail.com>
2026-05-06 14:26:11 +0530

    blog search features

--- blogs/views.py
+++ blogs/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Blog, Category
-
-# Create your views here.
+from django.db.models import Q
 
 def posts_by_category(request, id):
     posts = Blog.objects.filter(status='Published', category=id)
     try:
-        category = Category.objects.get(id=id) 
+        category = get_object_or_404(Category, id=id) 
     except:
         return redirect('home')
 
@@ -16,3 +15,20 @@ def posts_by_category(request, id):
         'category': category
     }
     return render(request, 'posts_by_category.html', context)
+
+def blogs(request,slug):
+    single_post = get_object_or_404(Blog, slug=slug, status='Published')
+    context = {
+        'single_post': single_post,
+    }
+    return render(request, 'blogs.html', context)
+
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status = 'Published' )
+    context = {
+        'blogs': blogs,
+        'keyword':keyword
+    }
+    return render(request, 'search.html',context)
