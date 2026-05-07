58cb7d179296952ce06a6afdba83fac8625e3667
abinjospj <abinjospj0722@gmail.com>
2026-05-07 15:59:05 +0530

    category CURD

--- blogs/views.py
+++ blogs/views.py
@@ -26,9 +26,11 @@ def blogs(request,slug):
 
 def search(request):
     keyword = request.GET.get('keyword')
-    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status = 'Published' )
+    
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status='Published')
+  
     context = {
         'blogs': blogs,
-        'keyword':keyword
+        'keyword': keyword,
     }
-    return render(request, 'search.html',context)
+    return render(request, 'search.html', context)
